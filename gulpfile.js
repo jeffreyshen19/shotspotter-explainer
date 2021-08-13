660698a50a598f188203624edd9433003e165f78
Jeffrey Shen <jjshen@mit.edu>
2021-08-12 22:19:29 -0700

    init website structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+var sass = require('gulp-sass')(require('node-sass'));
 var minify = require('gulp-minify');
-var child = require('child_process');
-var gutil = require('gulp-util');
 var babel = require('gulp-babel');
+var pug = require('gulp-pug');
+var webserver = require('gulp-webserver');
 
 gulp.task('babel', function(done) {
   return gulp.src(['./src/JS/*.js'])
@@ -16,8 +16,30 @@ gulp.task('babel', function(done) {
     .pipe(gulp.dest('./dist/JS/'));
 });
 
-gulp.task('watch', function() {
-  gulp.watch('./src/JS/*.js', ['babel']);
+gulp.task('views', function buildHTML() {
+  return gulp.src('src/views/*.pug')
+    .pipe(pug())
+    .pipe(gulp.dest('.'));
 });
 
-gulp.task("default", ["babel", "watch"]);
+gulp.task('sass', function () {
+  return gulp.src('src/SCSS/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(gulp.dest('dist/CSS'));
+});
+
+gulp.task('webserver', function() {
+  gulp.src('./')
+    .pipe(webserver({
+      livereload: true,
+      open: true
+    }));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./src/SCSS/*.scss', gulp.series('sass'));
+  gulp.watch('./src/views/*.pug', gulp.series('views'));
+  gulp.watch('./src/JS/*.js', gulp.series('babel'));
+});
+
+gulp.task('default', gulp.series('babel', 'views', 'sass', gulp.parallel('watch', 'webserver')));
